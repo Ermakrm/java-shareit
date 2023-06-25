d789fe0d8e574531af0be49990869c7f3423ba58
Ermakrm <ermakrm@yandex.ru>
2023-06-25 20:19:09 +0300

    ТЗ-15

--- src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
+++ src/test/java/ru/practicum/shareit/booking/BookingServiceIntegrationTest.java
@@ -7,10 +7,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
+import ru.practicum.shareit.booking.dto.BookingInfoDto;
+import ru.practicum.shareit.booking.exception.BookingNotFoundException;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.booking.service.BookingService;
+import ru.practicum.shareit.item.exception.ItemNotAvailableException;
+import ru.practicum.shareit.item.exception.ItemNotFoundException;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
 import ru.practicum.shareit.user.model.User;
@@ -20,8 +24,7 @@ import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 @TestPropertySource(properties = {"db.name=test"})
@@ -44,6 +47,9 @@ class BookingServiceIntegrationTest {
     Item item2;
     Item item3;
     Booking bookingWaiting;
+    Booking bookingFuture;
+    Booking bookingPast;
+    Booking bookingCurrent;
 
     @BeforeEach
     void setUp() {
@@ -85,7 +91,7 @@ class BookingServiceIntegrationTest {
         b1.setEnd(now.plusDays(20));
         b1.setItem(item1);
         b1.setBooker(user2);
-        Booking bookingFuture = bookingRepository.save(b1);
+        bookingFuture = bookingRepository.save(b1);
 
         Booking b2 = new Booking();
         b2.setStatus(Status.APPROVED);
@@ -93,7 +99,7 @@ class BookingServiceIntegrationTest {
         b2.setEnd(now.plusDays(1));
         b2.setItem(item2);
         b2.setBooker(user2);
-        Booking bookingCurrent = bookingRepository.save(b2);
+        bookingCurrent = bookingRepository.save(b2);
 
         Booking b3 = new Booking();
         b3.setStatus(Status.APPROVED);
@@ -101,7 +107,7 @@ class BookingServiceIntegrationTest {
         b3.setEnd(now.minusDays(10));
         b3.setItem(item3);
         b3.setBooker(user1);
-        Booking bookingPast = bookingRepository.save(b3);
+        bookingPast = bookingRepository.save(b3);
 
         Booking b4 = new Booking();
         b4.setStatus(Status.WAITING);
@@ -128,6 +134,31 @@ class BookingServiceIntegrationTest {
     }
 
     @Test
+    void create_whenItemNotAvailable_thenItemNotAvailableThrown() {
+        item1.setAvailable(false);
+        Booking b = new Booking();
+        b.setStart(now.plusHours(1));
+        b.setEnd(now.minusDays(1));
+        b.setBooker(user2);
+        b.setItem(item1);
+        b.setStatus(Status.WAITING);
+
+        assertThrows(ItemNotAvailableException.class, () -> bookingService.create(b, user2.getId()));
+    }
+
+    @Test
+    void create_whenOwnerTryToBook_thenItemNotFoundThrown() {
+        Booking b = new Booking();
+        b.setStart(now.plusHours(1));
+        b.setEnd(now.minusDays(1));
+        b.setBooker(user2);
+        b.setItem(item1);
+        b.setStatus(Status.WAITING);
+
+        assertThrows(ItemNotFoundException.class, () -> bookingService.create(b, item1.getOwner().getId()));
+    }
+
+    @Test
     void approve() {
         Booking result = bookingService.approve(bookingWaiting.getId(), user2.getId(), true);
 
@@ -135,6 +166,19 @@ class BookingServiceIntegrationTest {
         expected.setStatus(Status.APPROVED);
 
         assertEquals(expected, result);
+    }
+
+    @Test
+    void approve_whenStatusNotWaiting_thenItemNotAvailableThrown() {
+        bookingService.approve(bookingWaiting.getId(), user2.getId(), true);
+        assertThrows(ItemNotAvailableException.class,
+                () -> bookingService.approve(bookingWaiting.getId(), user2.getId(), true));
+    }
+
+    @Test
+    void approve_whenTryToApproveNotOwner_thenItemNotFoundThrown() {
+        assertThrows(ItemNotFoundException.class,
+                () -> bookingService.approve(bookingWaiting.getId(), user1.getId(), true));
     }
 
     @Test
@@ -156,20 +200,112 @@ class BookingServiceIntegrationTest {
     }
 
     @Test
-    void findByUserIdAndState() {
+    void findById_whenNotFound_thenBookingNotFoundThrown() {
+        assertThrows(BookingNotFoundException.class,
+                () -> bookingService.findById(100L));
+    }
+
+    @Test
+    void findByUserIdAndState_caseAll() {
         List<Booking> result = bookingService.findByUserIdAndState(user1.getId(), "ALL", 0, 5);
 
         assertFalse(result.isEmpty());
         assertEquals(2, result.size());
-
     }
 
     @Test
-    void findByOwnerIdAndState() {
+    void findByUserIdAndState_caseFuture() {
+        List<Booking> result = bookingService.findByUserIdAndState(user1.getId(), "FUTURE", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByUserIdAndState_casePast() {
+        List<Booking> result = bookingService.findByUserIdAndState(user1.getId(), "PAST", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByUserIdAndState_caseCurrent() {
+        List<Booking> result = bookingService.findByUserIdAndState(user2.getId(), "CURRENT", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByUserIdAndState_caseWaiting() {
+        List<Booking> result = bookingService.findByUserIdAndState(user1.getId(), "WAITING", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByOwnerIdAndState_caseAll() {
         List<Booking> result = bookingService.findByOwnerIdAndState(user1.getId(), "ALL", 0, 5);
 
         assertFalse(result.isEmpty());
         assertEquals(2, result.size());
+    }
+
+    @Test
+    void findByOwnerIdAndState_caseFuture() {
+        List<Booking> result = bookingService.findByOwnerIdAndState(user1.getId(), "FUTURE", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByOwnerIdAndState_casePast() {
+        List<Booking> result = bookingService.findByOwnerIdAndState(user2.getId(), "PAST", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByOwnerIdAndState_caseCurrent() {
+        List<Booking> result = bookingService.findByOwnerIdAndState(user1.getId(), "CURRENT", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findByOwnerIdAndState_caseWaiting() {
+        List<Booking> result = bookingService.findByOwnerIdAndState(user2.getId(), "WAITING", 0, 5);
+
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+    }
+
+    @Test
+    void findLastBookingByItemId() {
+        BookingInfoDto result = bookingService.findLastBookingByItemId(item2.getId());
+
+        assertNotNull(result);
+        assertEquals(2L, result.getId());
+    }
+
+    @Test
+    void findNextBookingByItemId() {
+        BookingInfoDto result = bookingService.findNextBookingByItemId(item1.getId());
+
+        assertNotNull(result);
+        assertEquals(1L, result.getId());
+    }
+
+    @Test
+    void hasUserBookedItem() {
+        Boolean result = bookingService.hasUserBookedItem(user1.getId(), item1.getId());
+
+        assertFalse(result);
     }
 
 }
